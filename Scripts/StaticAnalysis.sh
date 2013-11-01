a51450b63f1f94d29908c6db3ee7bf2cf37fb79d
Renzo Crisóstomo <ruenzuo@gmail.com>
2013-11-01 16:47:03 -0500

    Static analysis script updated

--- Scripts/StaticAnalysis.sh
+++ Scripts/StaticAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xcodebuild -workspace WeatherApp.xcworkspace -scheme WeatherApp clean build | tee xcodebuild.log
+xcodebuild -workspace WeatherApp.xcworkspace -scheme WeatherApp clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO | tee xcodebuild.log
 oclint-xcodebuild
 oclint-json-compilation-database -e Pods/** -- -report-type pmd -o Build/oclint.xml

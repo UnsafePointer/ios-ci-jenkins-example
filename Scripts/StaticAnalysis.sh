4dab84a31f99d69b84969226b757bd5f9a80d5b7
Renzo Crisóstomo <ruenzuo@gmail.com>
2013-11-01 20:20:10 -0500

    Static analysis script -max-priority updated

--- Scripts/StaticAnalysis.sh
+++ Scripts/StaticAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xcodebuild -workspace WeatherApp.xcworkspace -scheme WeatherApp clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO | tee xcodebuild.log
 oclint-xcodebuild
-oclint-json-compilation-database -e Pods/** -- -report-type pmd -o Build/oclint.xml
+oclint-json-compilation-database -e Pods/** -- -max-priority-1 1000 -max-priority-2 1000 -max-priority-3 1000 -report-type pmd -o Build/oclint.xml
